b8a44237cdc5e52df6cc19006733ded53837e7a7
Gen Urakawa <g.urakawa@gmail.com>
2024-05-24 16:26:31 +0900

    クラック検出できないよー

--- crack_detection.py
+++ crack_detection.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+
 class CrackDetection:
     def compute_threshold(self, image):
         crack_pixels = image[image < 127]
@@ -14,19 +15,24 @@ class CrackDetection:
         moments = cv2.moments(mask.astype(np.uint8))
         if moments['mu20'] + moments['mu02'] == 0:
             return 0
-        return ((moments['mu20'] - moments['mu02']) ** 2 + 4 * moments['mu11'] ** 2) / ((moments['mu20'] + moments['mu02']) ** 2)
+        eccentricity = ((moments['mu20'] - moments['mu02']) ** 2 + 4 * moments['mu11'] ** 2) / (
+                    (moments['mu20'] + moments['mu02']) ** 2)
+        return np.sqrt(1 - eccentricity)
 
     def detect_cracks(self, gray_image, filtered_gray_image, output_detected_image_path):
         threshold = self.compute_threshold(filtered_gray_image)
         cracks = gray_image < threshold
-        labeled_cracks, num_labels = cv2.connectedComponents(cracks.astype(np.uint8))
+        num_labels, labeled_cracks = cv2.connectedComponents(cracks.astype(np.uint8))
         result_image = cv2.cvtColor(filtered_gray_image, cv2.COLOR_GRAY2BGR)
+
         for label in range(1, num_labels):
-            mask = labeled_cracks == label
+            mask = (labeled_cracks == label).astype(np.uint8)
             area = np.sum(mask)
-            contours, _ = cv2.findContours(mask.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            perimeter = cv2.arcLength(contours[0], True)
-            eccentricity = self.compute_eccentricity(mask)
-            if area >= 100 and area / (perimeter ** 2) <= 1 and eccentricity >= 0.97:
-                result_image[mask] = [0, 0, 255]
+            contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            if len(contours) > 0:
+                perimeter = cv2.arcLength(contours[0], True)
+                eccentricity = self.compute_eccentricity(mask)
+                if area >= 100 and area / (perimeter ** 2) <= 1 and eccentricity >= 0.97:
+                    result_image[mask == 1] = [0, 0, 255]  # 赤色に設定
+
         cv2.imwrite(output_detected_image_path, result_image)
