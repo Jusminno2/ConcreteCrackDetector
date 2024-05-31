6011249d77085a53abb7c02732a7b07876fa1abb
Gen Urakawa <g.urakawa@gmail.com>
2024-05-31 15:23:28 +0900

    change derectory

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from crack_detection import CrackDetection
 
 def main():
     pic_name = input('Please enter the path of the image (without extension): ')
-    input_image_path = f'pictures/{pic_name}.jpg'
-    output_gray_image_path = f'pictures/o-{pic_name}.jpg'
-    output_filtered_image_path = f'pictures/o-m-{pic_name}.jpg'
-    output_binary_image_path = f'pictures/binary-{pic_name}.jpg'
-    output_detected_image_path = f'pictures/detected-{pic_name}.jpg'
+    input_image_path = f'pictures/sample/{pic_name}.jpg'
+    output_gray_image_path = f'pictures/mean_shift/o-{pic_name}.jpg'
+    output_filtered_image_path = f'pictures/grayscale/o-m-{pic_name}.jpg'
+    output_binary_image_path = f'pictures/binary/binary-{pic_name}.jpg'
+    output_detected_image_path = f'pictures/detected/detected-{pic_name}.jpg'
 
     gray_mean_filter = GrayMeanFilter()
     _, filtered_gray_image = gray_mean_filter.convert_image_to_grayscale_and_filter(input_image_path, output_gray_image_path, output_filtered_image_path)
